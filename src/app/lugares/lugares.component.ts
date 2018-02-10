91963f37586f5797eeffd0c167a8b61496d6744e
Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-02-10 10:47:12 +0100

    Pruebas rutas

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -10,13 +10,13 @@ export class LugaresComponent {
   nombre:string = '';
   apellido:string = '';
   lugares:any = [
-    {plan:'pagado', cercania: 1, distancia:1, active:true, nombre: "Empresa 1"},
-    {plan:'gratuito', cercania: 1, distancia:1.5, active:true, nombre: "Empresa 2"},
-    {plan:'gratuito', cercania: 2, distancia:5, active:true, nombre: "Empresa 3"},
-    {plan:'gratuito', cercania: 2, distancia:7, active:true, nombre: "Empresa 4"},
-    {plan:'gratuito', cercania: 3, distancia:10, active:false, nombre: "Empresa 5"},
-    {plan:'pagado', cercania: 3, distancia:40, active:true, nombre: "Empresa 6"},
-    {plan:'gratuito', cercania: 3, distancia:145, active:true, nombre: "Empresa 7"}
+    {id: 1, plan:'pagado', cercania: 1, distancia:1, active:true, nombre: "Empresa 1"},
+    {id: 2, plan:'gratuito', cercania: 1, distancia:1.5, active:true, nombre: "Empresa 2"},
+    {id: 3, plan:'gratuito', cercania: 2, distancia:5, active:true, nombre: "Empresa 3"},
+    {id: 4, plan:'gratuito', cercania: 2, distancia:7, active:true, nombre: "Empresa 4"},
+    {id: 5, plan:'gratuito', cercania: 3, distancia:10, active:false, nombre: "Empresa 5"},
+    {id: 6, plan:'pagado', cercania: 3, distancia:40, active:true, nombre: "Empresa 6"},
+    {id: 7, plan:'gratuito', cercania: 3, distancia:145, active:true, nombre: "Empresa 7"}
   ];
   lat:number = 39.8957581;
   lng:number = -6.4741936;
