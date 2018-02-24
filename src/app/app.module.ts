15be0bc25877eddb1dd5b3950db9ba92ed15802c
Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-02-24 20:12:32 +0100

    Pipe personalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/dat
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { CrearComponent } from './crear/crear.component';
 import { HttpModule } from '@angular/http';
+import { LinkifystrPipe } from './pipes/linkifystr.pipe';
 
 const appRoutes: Routes = [
   {path:'', component: LugaresComponent},
@@ -43,7 +44,8 @@ export const firebaseConfig = {
       DetalleComponent,
       LugaresComponent,
       ContactoComponent,
-      CrearComponent
+      CrearComponent,
+      LinkifystrPipe
   ],
   imports: [
     BrowserModule,
