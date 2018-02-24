8b04bdf1b1cf01b4639b22a60ef7ab35070bf086
Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-02-24 21:05:33 +0100

    Animaciones en Angular 4

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-lugares',
-  templateUrl: './lugares.component.html'
+  templateUrl: './lugares.component.html',
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0,
+        backgroundColor: 'green',
+        transform: 'rotate3d(0,0,0,0deg)'
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor: 'yellow',
+        transform: 'rotate3d(5,10,20,30deg)'
+      })),
+      transition('inicial => final', animate(1000)),
+      transition('final => inicial', animate(500))
+    ])
+  ]
 })
 export class LugaresComponent {
   listo = false;
+  state = 'inicial';
   num = 0;
   nombre:string = '';
   apellido:string = '';
@@ -15,7 +33,9 @@ export class LugaresComponent {
   lng:number = -6.4741936;
 
   lugares = null;
-
+  animar() {
+    this.state = (this.state === 'final') ? 'inicial' : 'final';
+  }
   constructor(private lugaresService: LugaresService){
     this.lugaresService.getLugares().valueChanges().subscribe(lugares => {
       this.lugares = lugares;
